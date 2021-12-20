8aaa42614c8e24b5918d8f3c1cbcc3d097ac53c6
Shubham Shrestha <shubhamshrestha25@gmail.com>
2021-12-20 18:05:38 +0545

    Footer

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import PizzaList from "../component/PizzaList";
 import Slider from "../component/Slider";
 import styles from "../styles/Home.module.css";
@@ -8,7 +7,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>PIZZO</title>
+        <title>LAMA PIZZO</title>
         <meta name="description" content="Best Pizza in Town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
